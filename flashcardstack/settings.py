3b9b5978c6d4204a8db00fe64491e67606373f72
TMaczek <tomek.maczo@onet.pl>
2022-04-23 19:34:08 +0200

    env added

--- flashcardstack/settings.py
+++ flashcardstack/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+$xu^y2t))-bts^c1+_n=5ok)qjj1t2s)_p9afnkgk+rkt((b7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['flashcardstack.herokuapp.com']
+ALLOWED_HOSTS = ['flashcardstack.herokuapp.com', 'localhost']
 
 
 # Application definition
