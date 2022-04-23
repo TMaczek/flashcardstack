67b3d47b95eefae603e4fbb2c43a0e8bcc3bc246
TMaczek <tomek.maczo@onet.pl>
2022-04-23 19:02:59 +0200

    github test

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<div>test test</div>')
+    return HttpResponse('<div>test test 3</div>')
