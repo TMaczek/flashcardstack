411c6d39532bb5993deca528015d26dacc721a7a
TMaczek <tomek.maczo@onet.pl>
2022-05-01 17:32:36 +0200

    branch test

--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<div>test test branchy</div>')
+    return HttpResponse('<div>test test branchy 2</div>')
