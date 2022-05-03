ce3e260acab4c46c3b8b4d922682f3104dbc493a
KamilKapron <kapron.kam@gmail.com>
2022-05-03 17:14:25 +0100

    Added User registration

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name = 'home'),
     path('register/', user_views.register, name = 'register'),
-    path('login/', user_views.login, name = 'login')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'login'),
+    path('logout/', auth_views.LoginView.as_view(template_name='users/logout.html'), name = 'logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+import sys
+sys.path.append("..")
+from flashcardstack.database import connection
+
+
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('login')
-            messages.success(request, f'{username} Created succesfuly')
+            username = form.cleaned_data.get('username')
+            login = form.cleaned_data.get('login')
+            password = form.cleaned_data.get('password')
+            DC = connection.DatabaseConnection()
+            DC.addUser(username, login, password)
+            messages.success(request, f'{username} Created succesfuly {login}, {password}')
             return redirect('login')
     else:
         form = UserRegisterForm()
