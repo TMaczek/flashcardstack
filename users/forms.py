211180f4c7cbff5d79b7748a88372ec7642c6529
TMaczek <tomek.maczo@onet.pl>
2022-06-12 17:59:41 +0200

    css - the beggining

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(forms.Form):
-    username = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100 , widget=forms.TextInput(attrs={'class': "form__input"}))
     login = forms.CharField(max_length=100)
     password = forms.CharField(max_length=100)
     
